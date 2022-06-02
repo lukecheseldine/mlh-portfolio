9a089dc1e943d781190be3d83653b23ba7dc6b2e
lukecheseldine <cheeseldine@gmail.com>
2022-06-02 19:49:31 -0400

    added all routes and skeleton templates. seperatedout css into different files and one main file

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="LMS Portfolio", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="LMS Portfolio", url=os.getenv("URL"))
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html', title="LMS Portfolio", url=os.getenv("URL"))
+
+@app.route('/map')
+def map():
+    return render_template('map.html', title="LMS Portfolio", url=os.getenv("URL"))
+
