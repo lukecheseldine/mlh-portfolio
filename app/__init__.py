1ad5d74314e8e3061d5af8ab4579aa1270e4620f
Samuel Emard-Thibault <92962712+SamEThibault@users.noreply.github.com>
2022-06-03 11:55:45 -0400

    added temporary home background image

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 data = open('app/static/data.json')
 data = json.load(data)
 
-
+# routes send the loaded json object "data" to display personal information
 @app.route('/')
 def index():
     return render_template('index.html', title="LMS Portfolio", url=os.getenv("URL"), data=data)
