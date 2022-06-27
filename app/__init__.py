d85b21c179dcf585ae36586e1559e1a70f483d47
lukecheseldine <cheeseldine@gmail.com>
2022-06-26 20:14:36 -0700

    api test script

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,36 @@ from dotenv import load_dotenv
 import folium
 import json
 from folium.plugins import FloatImage
+from peewee import *
+from datetime import datetime
+from playhouse.shortcuts import model_to_dict
+
 
 load_dotenv()
 app = Flask(__name__)
 data = open("app/static/data.json")
 data = json.load(data)
 
+mydb = \
+MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 # routes send the loaded json object "data" to display personal information
 @app.route("/")
@@ -69,3 +93,23 @@ def folium_map():
 
     folium_map.save("app/templates/map.html")
     return render_template("mapindex.html")
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    mydb.session_commit
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p) 
+            for p in 
+TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
