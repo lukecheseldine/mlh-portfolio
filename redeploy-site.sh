6a4d0f32d2883b2df2495ffed99c4158237de83e
lukecheseldine <cheeseldine@gmail.com>
2022-06-27 15:01:39 -0700

    add sql driver to startt up script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,6 +5,7 @@ cd mlh-portfolio
 git fetch && git reset origin/main --hard
 source venv/bin/activate
 pip install -r requirements.txt
+pip install pymysql
 tmux new-session -d -s portfolio
 tmux send -t portfolio source SPACE venv/bin/activate ENTER
 tmux send -t portfolio flask SPACE run SPACE --host=0.0.0.0 ENTER
